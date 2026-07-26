c1fcab20d4381bbcc1b041b4e762fcf718817963
Seif <42210362.sief@acu.edu.eg>
2026-07-27 00:44:32 +0300

    Add Two Controller

---
+++ WebApplication1/WebApplication1/Controllers/ItemController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task1.Repos;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly ItemsRepo _repo;
+        public ItemController(ItemsRepo repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(_repo.GetAll());
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(_repo.GetById(id));
+        }
+
+    }
+}
